3d6f279023f3d9ff54ceaf49bed595a7aaa92fc2
eczhang-rzri <emily.zhang@utexas.edu>
2025-04-29 21:03:34 -0500

    home test with alternative .env setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  define: {
+    'process.env.API_URL': JSON.stringify('https://simpleval-api.azurewebsites.net'),
+  }
 })
