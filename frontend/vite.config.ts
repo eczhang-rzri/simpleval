1c7bc07040c55c46ea20df2cb3d64ab8c4485a9d
eczhang-rzri <emily.zhang@utexas.edu>
2025-04-29 21:17:56 -0500

    Revert "home test with alternative .env setup"
    This reverts commit 3d6f279023f3d9ff54ceaf49bed595a7aaa92fc2.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  define: {
-    'process.env.API_URL': JSON.stringify('https://simpleval-api.azurewebsites.net'),
-  }
 })
